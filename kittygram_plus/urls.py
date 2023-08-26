2d416e439ab3420236dc97c975a5add917472ed6
Владимир Богачёв <bogachevvova@gmail.com>
2023-08-27 01:52:33 +0300

    v4

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -12,4 +12,6 @@ router.register(r'owners', OwnerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt'))
 ]
